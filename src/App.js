28da25bf0d263a7f9383336b2235e4870e3569bc
Chris Eld <chris@chriseld.com>
2021-02-19 11:27:28 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,173 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class SendButton extends Component{
+  render(){
+    return (<div className="send_message" onClick={this.props.handleClick}>
+      <div className="text">send</div>
+    </div>);
+  }
+}
+
+class MessageTextBoxContainer extends Component{
+  render(){
+    return(
+      <div className="message_input_wrapper">
+        <input id="msg_input" className="message_input" placeholder="Type your messages here..." value={this.props.message} onChange={this.props.onChange} onKeyPress={this.props._handleKeyPress}/>
+      </div>
+    );
+  }
+}
+
+class Avatar extends Component {
+  render(){
+    return(
+      <div className="avatar"/>
+    );
+  }
+}
+
+class BotMessageBox extends Component{
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+      <li className="message left appeared">
+        <Avatar></Avatar>
+        <div className="text_wrapper">
+            <div className="text">{this.props.message}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+class UserMessageBox extends Component{
+  constructor(props) {
+    super(props);
+
+  }
+  render(){
+    return(
+      <li className={`message ${this.props.appearance} appeared`}>
+        <Avatar></Avatar>
+        <div className="text_wrapper">
+            <div className="text">{this.props.message}</div>
+        </div>
+      </li>
+    );
+  }
+}
+
+class MessagesContainer extends Component{
+  constructor(props) {
+    super(props);
+    this.createBotMessages = this.createBotMessages.bind(this);
+  }
+
+  scrollToBottom = () => {
+    var el = this.refs.scroll;
+    el.scrollTop = el.scrollHeight;
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  createBotMessages(){
+    console.log(this.props.messages);
+    return this.props.messages.map((message, index) =>
+       <UserMessageBox key={index} message={message["message"]} appearance={message["isbotmessage"] ? "left": "right"}/>
+    );
+  }
+
+  render(){
+
+    return(
+      <ul className="messages" ref="scroll">
+        {this.createBotMessages()}
+      </ul>
+    );
+  }
+}
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {"messages": [], "current_message":""}
+    this.handleClick = this.handleClick.bind(this);
+    this._handleKeyPress = this._handleKeyPress.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.addMessageBox = this.addMessageBox.bind(this);
+  }
+  
+
+  addMessageBox(enter=true){
+    let messages = this.state.messages;
+    let current_message = this.state.current_message;
+    console.log(this.state);
+    if(current_message && enter){
+      messages = [...messages, {"message":current_message}];
+      fetch("http://localhost:3000?message=" + current_message)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          this.setState({
+            messages: [...messages, {"message":result["message"], "isbotmessage":true}]
+          });
+        },
+        (error) => {
+          //do nothing for now
+        }
+      );
+      current_message = ""
+    }  
+    this.setState({
+      current_message: current_message,
+      messages
+    });
+
+  }
+
+  handleClick(){
+    this.addMessageBox();
+  }
+
+  onChange(e) {
+    this.setState({
+      current_message: e.target.value
+    });  
+  }
+
+    _handleKeyPress(e) {
+    let enter_pressed = false;
+    if(e.key === "Enter"){
+      enter_pressed = true;
+    }
+    this.addMessageBox(enter_pressed)
+  }
+
+  render() {
+    return (
+      <div className="chat_window">
+        <MessagesContainer messages={this.state.messages}></MessagesContainer>
+        <div className="bottom_wrapper clearfix">
+          <MessageTextBoxContainer 
+            _handleKeyPress={this._handleKeyPress} 
+            onChange={this.onChange} 
+            message={this.state.current_message}></MessageTextBoxContainer>
+          <SendButton handleClick={this.handleClick}></SendButton>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
